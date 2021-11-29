ef2e6072feb41461ec3e016580fae7e832f67204
mathis-m <mathis.michel@outlook.de>
2021-11-29 07:03:54 +0100

    feat(ui): add momentjs

--- src/ui/src/index.js
+++ src/ui/src/index.js
@@ -6,6 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import store from "./redux/store";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import localization from 'moment/locale/de';
+import moment from "moment";
+
+moment.updateLocale('de', localization);
+moment.locale('de');
 
 ReactDOM.render(
     <Provider store={store}>

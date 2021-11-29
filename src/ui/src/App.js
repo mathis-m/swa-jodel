fcc402790f5713e7ce9e2d8c5e823caea12e88e0
mathis-m <mathis.michel@outlook.de>
2021-11-29 07:04:08 +0100

    feat(ui): add new routes

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -4,8 +4,11 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import React, {lazy, Suspense} from "react";
 import {CircularProgress} from "@mui/material";
 import Footer from "./layout/footer/footer";
+import AddPost from "./components/add-post/add-post";
 
-const NewestPosts = lazy(() => import("./layout/pages/newest-posts/newest-posts"));
+const NewestPosts = lazy(() => import("./layout/pages/latest-posts/latest-posts"));
+const MostCommentsPosts = lazy(() => import("./layout/pages/most-comments-posts/most-comments-posts"));
+const HighestVoting = lazy(() => import("./layout/pages/highest-voting-posts/highest-voting"));
 
 function App() {
     return (
@@ -15,7 +18,14 @@ function App() {
                 <Switch>
                     <Route exact path="/" component={NewestPosts}/>
                 </Switch>
+                <Switch>
+                    <Route exact path="/most-comments" component={MostCommentsPosts}/>
+                </Switch>
+                <Switch>
+                    <Route exact path="/highest-voting" component={HighestVoting}/>
+                </Switch>
             </Suspense>
+            <AddPost/>
             <Footer/>
         </Router>
     );

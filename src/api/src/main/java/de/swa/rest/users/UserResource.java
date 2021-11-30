c7ae315253878269d95260d0020e5c05a17d52f3
mathis-m <mahtis.michel@outlook.de>
2021-11-30 17:03:55 +0100

    feat(api): provide user register local / google endpoints

--- src/api/src/main/java/de/swa/rest/users/UserResource.java
+++ src/api/src/main/java/de/swa/rest/users/UserResource.java
@@ -1,35 +1,117 @@
 package de.swa.rest.users;
 
-import de.swa.infrastructure.dao.UserDao;
-import de.swa.infrastructure.entities.UserEntity;
-import de.swa.rest.users.dto.CreateUserDto;
-import io.quarkus.security.identity.SecurityIdentity;
-import org.jboss.resteasy.annotations.cache.NoCache;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import de.swa.auth.GoogleTokenValidator;
+import de.swa.infrastructure.repositories.UserRepository;
+import de.swa.infrastructure.repositories.exceptions.UniqueExternalIdRequiredException;
+import de.swa.infrastructure.repositories.exceptions.UniqueUserNameRequiredException;
+import de.swa.rest.users.dto.CreateExternalUserDto;
+import de.swa.rest.users.dto.CreateLocalUserDto;
+import de.swa.rest.users.dto.UserResponseDto;
+import de.swa.rest.users.dto.factories.UserEntityToResponseDtoFactory;
+import io.quarkus.security.Authenticated;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirements;
+import org.jboss.resteasy.reactive.RestHeader;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 
-import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestScoped
 @Path("/users")
 public class UserResource {
     @Inject
-    UserDao userDao;
-   
+    UserRepository userRepository;
+
+    @Inject
+    GoogleTokenValidator googleTokenValidator;
+
+    @Authenticated
+    @SecurityRequirements({
+            @SecurityRequirement(
+                    name = "Google"
+            ),
+            @SecurityRequirement(
+                    name = "Local"
+            )
+    })
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<UserEntity> getUsers() {
-        return userDao.getAllUsers();
+    public RestResponse<List<UserResponseDto>> getUsers() {
+
+        var entities = userRepository.listAll();
+        var returnDto = entities
+                .stream()
+                .map(UserEntityToResponseDtoFactory::map)
+                .collect(Collectors.toList());
+
+        return ResponseBuilder
+                .ok(returnDto, MediaType.APPLICATION_JSON_TYPE)
+                .build();
     }
 
-    @POST
+    @POST()
+    @Path("/register/local")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public RestResponse<String> createUser(CreateLocalUserDto user) {
+        try {
+            userRepository.createLocalUser(user.getUsername(), user.getPassword());
+        } catch (UniqueUserNameRequiredException userNameNotUnique) {
+            return ResponseBuilder
+                    .create(RestResponse.Status.CONFLICT, userNameNotUnique.getMessage())
+                    .build();
+        }
+        return ResponseBuilder
+                .ok("User created!", MediaType.TEXT_PLAIN)
+                .status(RestResponse.Status.CREATED)
+                .build();
+    }
+
+    @SecurityRequirement(
+            name = "Google"
+    )
+    @POST()
+    @Path("/register/google")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public UserEntity createUser(CreateUserDto user){
-        var temp = new UserEntity(user.getUsername());
-        return userDao.addUser(temp);
+    public RestResponse<String> createUserByGoogle(
+            CreateExternalUserDto user,
+            @RestHeader("Authorization") String authHeader
+    ) {
+        RestResponse<String> provideValidGoogleBearer = ResponseBuilder
+                .create(RestResponse.Status.UNAUTHORIZED, "Please provide valid Google Bearer")
+                .build();
+        if (authHeader == null) {
+            return provideValidGoogleBearer;
+        }
+        var token = authHeader.replace("Bearer ", "");
+        GoogleIdToken googleIdToken = null;
+        try {
+            googleIdToken = googleTokenValidator.yodelVerifier.verify(token);
+        } catch (Exception e) {
+            return provideValidGoogleBearer;
+        }
+        if (googleIdToken == null) {
+            return provideValidGoogleBearer;
+        }
+
+        try {
+            userRepository.createExternalUser(user.getUsername(), googleIdToken.getPayload().getSubject());
+        } catch (UniqueUserNameRequiredException | UniqueExternalIdRequiredException uniqueException) {
+            return ResponseBuilder
+                    .create(RestResponse.Status.CONFLICT, uniqueException.getMessage())
+                    .build();
+        }
+
+        return ResponseBuilder
+                .ok("Google user successful registered!", MediaType.TEXT_PLAIN)
+                .build();
     }
 }

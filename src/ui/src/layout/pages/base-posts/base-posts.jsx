46235cb990167749d8880ebcc84d2c0f185f7b14
rolait01 <rolait01@hs-esslingen.de>
2021-12-01 07:08:28 +0100

    feat(ui): added Infinite scroll

--- src/ui/src/layout/pages/base-posts/base-posts.jsx
+++ src/ui/src/layout/pages/base-posts/base-posts.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./base-posts.scss"
 import {Col, Container, Row} from "react-bootstrap";
 import Post from "../../../components/post/post";
 import {usePosts} from "../../../redux/features/latestPostSlice";
+import {Button, CircularProgress} from "@mui/material";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const colorArray = [
     '#FF6633',
@@ -32,21 +34,34 @@ const colorArray = [
     '#FF4D4D',
 ];
 
-const BasePosts = ({posts}) => {
+const BasePosts = ({posts, hasMore, fetchMoreData, listKey}) => {
     return (
-        <Container className="page">
-            <Row style={{alignContent: "flex-start"}}>
-                {
-                    posts.map((p, i) => (
-                        <Col key={"post-id-" + p.id} sm={12} md={4} className="post-container">
-                            <Post id={p.id} color={colorArray[p.id % colorArray.length]} text={p.text} user={p.user}
-                                  date={p.date} commentCount={p.comments.length}
-                                  locationText={p.locationText}/>
-                        </Col>
-                    ))
-                }
-            </Row>
-        </Container>
+        <div className="page container">
+            <div style={{height: "100%"}}>
+                <InfiniteScroll
+                    className="row m-0"
+                    height="100%"
+                    dataLength={posts.length}
+                    next={fetchMoreData}
+                    hasMore={hasMore}
+                    loader={<div className="loader-wrapper"><CircularProgress/></div>}
+                    endMessage={"Ende der Liste erreicht!"}>
+                    {
+                        posts.map((p) => (
+                            <Col key={listKey + "-post-id-" + p.id} sm={12} md={4} className="post-container">
+                                <Post id={p.id} color={colorArray[p.id % colorArray.length]} text={p.text} user={p.user}
+                                      date={p.createdAt} commentCount={p.commentCount}
+                                      locationText={p.locationText}/>
+                            </Col>
+                        ))
+                    }
+                    {
+                        hasMore && <div className="load-more-wrapper"><Button onClick={fetchMoreData}>Mehr anzeigen</Button>
+                        </div>
+                    }
+                </InfiniteScroll>
+            </div>
+        </div>
     )
 }
 export default BasePosts;

12861a6a49787169c445706f8d3b9b614b508c32
rolait01 <rolait01@hs-esslingen.de>
2021-12-01 07:07:36 +0100

    feat(ui): prepare post vote from api but not working

--- src/ui/src/components/post-vote/post-vote.jsx
+++ src/ui/src/components/post-vote/post-vote.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
 import UpVoteIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 import DownVoteIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 
 import "./post-vote.scss"
 import {IconButton} from "@mui/material";
 import {useDispatch} from "react-redux";
-import {postDownVoted, postUpVoted, useVoting} from "../../redux/features/votingSlice";
+import {
+    fetchVotingForPost,
+    postDownVoted,
+    postUpVoted,
+    sendPostDownVoted,
+    sendPostUpVoted,
+    useVoting
+} from "../../redux/features/votingSlice";
+import {useIsLoggedIn} from "../../redux/features/userSlice";
 
 const PostVote = ({postId}) => {
+    const isLoggedIn = useIsLoggedIn();
     const dispatch = useDispatch();
     const {hasUpVoted, hasDownVoted, voteCount} = useVoting(postId);
+    useEffect(() => {
+        //if (isLoggedIn)
+            // dispatch(fetchVotingForPost(postId));
+    }, [dispatch, isLoggedIn]);
     let sxUp = {color: hasUpVoted ? "#7A9F35" : "white"};
     let sxDown = {color: hasDownVoted ? "#E7410FFF" : "white"};
     const withStroke = {
@@ -18,28 +31,44 @@ const PostVote = ({postId}) => {
         strokeLinecap: "round",
         strokeLinejoin: "bevel"
     }
-    if(hasUpVoted) {
+    if (!isLoggedIn) {
+        sxUp.color = "#dcdcdc"
+        sxDown.color = "#dcdcdc"
+    }
+    if (hasUpVoted) {
         sxUp = {
             ...sxUp,
             ...withStroke
         }
     }
-    if(hasDownVoted) {
+    if (hasDownVoted) {
         sxDown = {
             ...sxDown,
             ...withStroke
         }
     }
+    const onUpVote = () => {
+        if (!isLoggedIn)
+            return;
+        dispatch(postUpVoted(postId));
+        //dispatch(sendPostUpVoted(postId));
+    };
+    const onDownVote = () => {
+        if (!isLoggedIn)
+            return;
+        dispatch(postDownVoted(postId));
+        //dispatch(sendPostDownVoted(postId));
+    }
     return (
         <div className="post-vote">
-            <IconButton sx={sxUp} onClick={() => dispatch(postUpVoted(postId))}>
+            <IconButton sx={sxUp} onClick={onUpVote} disabled={!isLoggedIn}>
                 <UpVoteIcon sx={{fontSize: "48px"}}/>
             </IconButton>
             <div className="vote-count">
                 {voteCount}
             </div>
-            <IconButton sx={sxDown} onClick={() => dispatch(postDownVoted(postId))}>
-                <DownVoteIcon sx={{fontSize: "48px" }}/>
+            <IconButton sx={sxDown} onClick={onDownVote} disabled={!isLoggedIn}>
+                <DownVoteIcon sx={{fontSize: "48px"}}/>
             </IconButton>
         </div>
     )

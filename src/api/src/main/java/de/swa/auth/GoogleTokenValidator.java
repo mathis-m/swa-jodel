96dd88d42c659cdda7f7d0d7e9f2e7548df51d4f
mathis-m <mahtis.michel@outlook.de>
2021-12-01 07:00:00 +0100

    chore(api): update client id

--- src/api/src/main/java/de/swa/auth/GoogleTokenValidator.java
+++ src/api/src/main/java/de/swa/auth/GoogleTokenValidator.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 @ApplicationScoped
 public class GoogleTokenValidator {
     public GoogleIdTokenVerifier yodelVerifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-            .setAudience(Collections.singletonList("541104707781-99oiptsrmgb78nkdhp3mngjkov9vm3ae.apps.googleusercontent.com"))
-            .setIssuer("https://accounts.google.com")
+            .setAudience(Collections.singletonList("978825777932-3dgb9vtadeno8mbvs763i6tb138tuu0f.apps.googleusercontent.com"))
             .build();
 }
